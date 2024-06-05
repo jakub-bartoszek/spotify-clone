4cb085252faecbbb19e2c57fb4d93d3ad4e2d76a
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-05 12:47:53 +0200

    add: created header component

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header";
+
+export default function Home() {
+ return (
+  <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+   <Header />
+   Main content
+  </div>
+ );
+}
