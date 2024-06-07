b426a3e1ab2f3482d6f6d755083641bbc75d3945
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-07 11:26:44 +0200

    change: changed overflow

--- components/library/library.tsx
+++ components/library/library.tsx
@@ -26,7 +26,7 @@ export const Library = ({ songs }: LibraryProps) => {
  };
 
  return (
-  <div className="flex flex-col">
+  <div className="flex flex-col h-full overflow-hidden">
    <div className="flex items-center justify-between px-5 pt-4">
     <div className="inline-flex items-center gap-x-2">
      <TbPlaylist
@@ -41,7 +41,7 @@ export const Library = ({ songs }: LibraryProps) => {
      className="text-neutral-400 cursor-pointer hover:text-white transition"
     />
    </div>
-   <div className="flex flex-col gap-y-2 mt-4 px-3">
+   <div className="flex flex-col gap-y-2 mt-4 px-3 h-full overflow-hidden overflow-y-auto">
     {songs.map((item) => (
      <LibraryItem
       onClick={() => {}}
