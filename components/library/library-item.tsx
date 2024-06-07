524e2ebf957539e0b4c1b5f1c4b32e4ba92296e4
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-07 11:27:57 +0200

    add: created library item

---
+++ components/library/library-item.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types/types";
+import Image from "next/image";
+import { IoMusicalNotes } from "react-icons/io5";
+
+interface LibraryItemProps {
+ data: Song;
+ onClick: (id: string) => void;
+}
+
+export const LibraryItem = ({ data, onClick }: LibraryItemProps) => {
+ const imagePath = useLoadImage(data);
+
+ const handleClick = () => {
+  if (onClick) {
+   return onClick(data.id);
+  }
+ };
+
+ return (
+  <div
+   onClick={handleClick}
+   className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+  >
+   <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+    {imagePath ? (
+     <Image
+      className="object-cover"
+      src={imagePath || "/images/note.png"}
+      fill
+      alt="Image"
+     />
+    ) : (
+     <div className="bg-neutral-400/10 h-full flex items-center justify-center text-neutral-500">
+      <IoMusicalNotes size={75} />
+     </div>
+    )}
+   </div>
+   <div className="flex flex-col gap-y-1 overflow-hidden">
+    <p className="text-white truncate">{data.title}</p>
+    <p className="text-neutral-400 text-sm truncate">
+     {data.author}
+    </p>
+   </div>
+  </div>
+ );
+};
+
+export default LibraryItem;
